6f026149302770080509109d2305411509ddedda
Alea Farrel <aleafarrel4@gmail.com>
2026-05-20 17:15:26 +0700

    Bump app version to 1.9-stable

--- src/js/config/version-config.js
+++ src/js/config/version-config.js
@@ -5,7 +5,7 @@
 
 export const CONFIG = {
     appName: 'Satu Ramadhan',
-    version: '1.8-stable',
+    version: '1.9-stable',
     developer: 'Alea Farrel',
     privacyPolicyUrl: 'https://saturamadhan-policy.afarrel.workers.dev/'
 };
